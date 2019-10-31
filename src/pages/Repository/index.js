3f315ed2eea0b3b6da755da1cf7a4e5097298368
sirmcclay <mcclay.design@gmail.com>
2019-10-31 06:08:42 -0300

    Topcis - 13 done 14 * 0.65 - continue...

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -52,10 +53,26 @@ export default class Repository extends Component {
     return (
       <Container>
         <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {/** LABELS */}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -15,6 +15,12 @@ export const Owner = styled.div`
   flex-direction: column;
   align-items: center;
 
+  a {
+    color: #7159c1;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
   img {
     width: 120px;
     border-radius: 50%;
@@ -33,5 +39,48 @@ export const Owner = styled.div`
     line-height: 1.4;
     text-align: center;
     max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+  }
+
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 2px solid #eee;
+  }
+
+  div {
+    flex: 1;
+    margin-left: 15px;
+
+    strong {
+      font-size: 16px;
+
+      a {
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+          color: #7159c1;
+        }
+      }
+    }
   }
 `;
